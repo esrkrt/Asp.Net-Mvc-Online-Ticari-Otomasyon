5783f3c60aceb0ee38c054826dcfd7b2469c8f73
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-09 01:40:01 +0300

    ToList metodu ile kategori listeleme

--- MvcTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcTicariOtomasyon/Controllers/KategoriController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcTicariOtomasyon.Models.Siniflar;
 namespace MvcTicariOtomasyon.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
+
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Kategoris.ToList();
+
+            return View(degerler);
         }
     }
 }
