85164c84d62b9df73c5d3698a1634823d2d5e85b
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-09 00:54:16 +0300

    entitiy framework code first yapısı ile, ilgili veri tabanı oluşturuldu.

---
+++ MvcTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Siniflar
+{
+    public class Faturalar
+    {
+        [Key]
+        public int Faturaid { get; set; }
+
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
+        public string FaturaSeriNo { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(6)]
+        public string FaturaSıraNo { get; set; }
+
+        public DateTime Tarih { get; set; }
+        public DateTime Saat { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(60)]
+        public string VergiDairesi { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string TeslimEden { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string TeslimAlan { get; set; }
+        public ICollection<FaturaKalem> FaturaKalems { get; set; }
+    }
+}
---
+++ MvcTicariOtomasyon/Models/Siniflar/Gider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Siniflar
+{
+    public class Gider
+    {
+        [Key]
+        public int Giderid { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string Aciklama { get; set; }
+
+        public DateTime Tarih { get; set; }
+        public DateTime Tutar { get; set; }
+
+    }
+}
