b291337a19ef47225358b00481c9492f147d96d9
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-09 01:48:11 +0300

    remove metodu ile kategori silme işlemi yapıldı

--- MvcTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcTicariOtomasyon/Controllers/KategoriController.cs
@@ -29,5 +29,12 @@ namespace MvcTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
