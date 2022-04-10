6536bcdd9985a78e684dbf49a5945faeed71c291
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-10 22:05:13 +0300

    urunleri guncelleme sayfasına taşıma ve guncelleme işlemi yapıldı

--- MvcTicariOtomasyon/Controllers/UrunController.cs
+++ MvcTicariOtomasyon/Controllers/UrunController.cs
@@ -42,5 +42,35 @@ namespace MvcTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> kategoriler = (from x in c.Kategoris.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.KategoriAd,
+                                                    Value = x.KategoriID.ToString()
+                                                }).ToList();
+            ViewBag.ktgr = kategoriler;
+
+            var urundeger = c.Uruns.Find(id);
+            return View("UrunGetir", urundeger);
+        }
+        public ActionResult UrunGuncelle(Urun u)
+        {
+            var urn = c.Uruns.Find(u.UrunID);
+
+            urn.AlisFiyat = u.AlisFiyat;
+            urn.Durum = u.Durum;
+            urn.KategoriID = u.KategoriID;
+            urn.Marka = u.Marka;
+            urn.SatisFiyat = u.SatisFiyat;
+            urn.Stok = u.Stok;
+            urn.UrunAd = u.UrunAd;
+            urn.UrunGorsel = u.UrunGorsel;
+
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
