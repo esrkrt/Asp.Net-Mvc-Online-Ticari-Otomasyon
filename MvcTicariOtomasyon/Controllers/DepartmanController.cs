35aa5c18dbc2d23f52b1a5f6fcd5ae3f0746d6af
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-10 23:12:42 +0300

    departman listeleme işlemi yapıldı

---
+++ MvcTicariOtomasyon/Controllers/DepartmanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTicariOtomasyon.Models.Siniflar;
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class DepartmanController : Controller
+    {
+
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var departmanlar = c.Departmans.ToList();
+            return View(departmanlar);
+        }
+    }
+}
