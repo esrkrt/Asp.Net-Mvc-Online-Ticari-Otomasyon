c1807dc4365991298e06684d93afb1babcb5e0ad
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-13 14:48:07 +0300

    yeni cari ekleme sayfa tasarımı ve işlemi

--- MvcTicariOtomasyon/Controllers/CariController.cs
+++ MvcTicariOtomasyon/Controllers/CariController.cs
@@ -17,5 +17,17 @@ namespace MvcTicariOtomasyon.Controllers
             var degerler = c.Carilers.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult YeniCari()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCari(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
