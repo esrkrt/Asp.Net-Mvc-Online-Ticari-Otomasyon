25eb930b39a8dfc15467756ccbadd9996661d6cb
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-13 16:50:19 +0300

    yeni Personel ekleme sayfa tasarımı ve işlemi

--- MvcTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcTicariOtomasyon/Controllers/PersonelController.cs
@@ -15,5 +15,26 @@ namespace MvcTicariOtomasyon.Controllers
             var degerler = c.Personels.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> departmanlar = (from x in c.Departmans.ToList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.DepartmaAd,
+                                                     Value = x.Departmanid.ToString()
+                                                 }).ToList();
+            ViewBag.dprtmn = departmanlar;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            p.Durum = true;
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
