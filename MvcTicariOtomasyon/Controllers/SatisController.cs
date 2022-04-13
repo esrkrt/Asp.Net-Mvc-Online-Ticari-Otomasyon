d83bae3a18bc45a22e4ca3e88c118b6c66dde1e7
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-13 21:19:51 +0300

    satis lıstesi işlemi ve arayüz tasarımı v

---
+++ MvcTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTicariOtomasyon.Models.Siniflar;
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+    }
+}
