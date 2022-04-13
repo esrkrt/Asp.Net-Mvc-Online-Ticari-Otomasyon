20294a915faa94fc7aa315598716652b691f66a0
ESRA KURT <esra.kurt97833@gmail.com>
2022-04-13 21:37:42 +0300

    yeni satış yapma işlemi yapıldı

--- MvcTicariOtomasyon/Controllers/SatisController.cs
+++ MvcTicariOtomasyon/Controllers/SatisController.cs
@@ -15,5 +15,40 @@ namespace MvcTicariOtomasyon.Controllers
             var degerler = c.SatisHarekets.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult SatisEkle()
+        {
+            List<SelectListItem> urunler = (from x in c.Uruns.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.UrunAd,
+                                                Value = x.UrunID.ToString()
+                                            }).ToList();
+            List<SelectListItem> cariler = (from x in c.Carilers.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.CariAd + " " + x.CariSoyad,
+                                                Value = x.Cariid.ToString()
+                                            }).ToList();
+            List<SelectListItem> personeller = (from x in c.Personels.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                                    Value = x.PersonelID.ToString()
+                                                }).ToList();
+            ViewBag.urun = urunler;
+            ViewBag.cari = cariler;
+            ViewBag.personel = personeller;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SatisEkle(SatisHaraket s)
+        {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
