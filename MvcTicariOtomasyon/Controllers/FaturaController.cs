18d2e354bd652c947de8b8528d30912b5f28d593
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-09 14:21:06 +0300

    fatura detayi getirildi

--- MvcTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcTicariOtomasyon/Controllers/FaturaController.cs
@@ -27,5 +27,10 @@ namespace MvcTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult FaturaGetir(int id)
+        {
+            var fatura = c.Faturalars.Find(id);
+            return View("FaturaGetir", fatura);
+        }
     }
 }
