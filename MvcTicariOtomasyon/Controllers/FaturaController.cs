71002d6ff496564f9adce5e26ee685c2629cd440
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-09 13:40:57 +0300

    fatura listeleme islemi yapildi

---
+++ MvcTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTicariOtomasyon.Models.Siniflar;
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var faturalar = c.Faturalars.ToList();
+            return View(faturalar);
+        }
+    }
+}
