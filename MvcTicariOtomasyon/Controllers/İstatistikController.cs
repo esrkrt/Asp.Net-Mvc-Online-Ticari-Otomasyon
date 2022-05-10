7a165ab80c658de70751ff545573a042065e6d5c
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-10 18:27:35 +0300

    iststistik formunun tasarimi yapildi

---
+++ MvcTicariOtomasyon/Controllers/İstatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class İstatistikController : Controller
+    {
+        // GET: İstatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
