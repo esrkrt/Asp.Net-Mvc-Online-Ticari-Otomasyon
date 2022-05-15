bdad18a8a192828dcddc440d593daa608e7fef6f
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-16 00:52:57 +0300

    musteri sehir tablosu düzenlendi

--- MvcTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcTicariOtomasyon/Controllers/İstatistikController.cs
@@ -65,5 +65,16 @@ namespace MvcTicariOtomasyon.Controllers
 
             return View();
         }
+        public ActionResult KolayTablolar()
+        {
+            var sorgu = from x in c.Carilers
+                        group x by x.CariSehir into g
+                        select new SinifGrup
+                        {
+                            Sehir = g.Key,
+                            Sayi = g.Count()
+                        };
+            return View(sorgu.ToList());
+        }
     }
 }
