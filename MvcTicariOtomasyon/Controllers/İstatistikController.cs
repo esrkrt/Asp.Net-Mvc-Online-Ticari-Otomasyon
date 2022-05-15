1821d0639894c6aab46ae96e6809a0f6d132dff5
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-16 01:26:54 +0300

    Departman personel tablosu düzenlendi

--- MvcTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcTicariOtomasyon/Controllers/İstatistikController.cs
@@ -76,5 +76,18 @@ namespace MvcTicariOtomasyon.Controllers
                         };
             return View(sorgu.ToList());
         }
+        public PartialViewResult Partial1()
+        {
+            var sorgu2 = from x in c.Personels
+                         group x by x.DepartmanID into g
+                         select new SinifGrup2
+                         {
+                             Departman = g.Key,
+                             Sayi = g.Count()
+
+                         };
+            return PartialView(sorgu2.ToList());
+        }
+
     }
 }
---
+++ MvcTicariOtomasyon/Models/Siniflar/SinifGrup2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Siniflar
+{
+    public class SinifGrup2
+    {
+        public int Departman { get; set; }
+        public int Sayi{ get; set; }
+    }
+}
