ee738646e1c8dbcaf5e7fd817c877195084e83cc
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-15 15:06:45 +0300

    urun detay sayfasi tasarimi için tema eklendi

---
+++ MvcTicariOtomasyon/Controllers/UrunDetayiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class UrunDetayiController : Controller
+    {
+        // GET: UrunDetayi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
