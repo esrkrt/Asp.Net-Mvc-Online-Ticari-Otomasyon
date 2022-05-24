929e031aaab192a65bd2db79edd822544b3a8efa
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-24 12:36:21 +0300

    to do list kartlara veriler çekildi

--- MvcTicariOtomasyon/Controllers/YapilacakController.cs
+++ MvcTicariOtomasyon/Controllers/YapilacakController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcTicariOtomasyon.Models.Siniflar;
 namespace MvcTicariOtomasyon.Controllers
 {
     public class YapilacakController : Controller
     {
         // GET: Yapilacak
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var deger1 = c.Carilers.Count().ToString();
+            ViewBag.cari = deger1;
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.urun = deger2;
+            var deger3 = c.Kategoris.Count().ToString();
+            ViewBag.ktg = deger3;
+            var deger4 = (from x in c.Carilers select x.CariSehir).Distinct().Count().ToString();
+            ViewBag.sehir = deger4;
+            var yapilacaklar = c.Yapilacaks.ToList();
+            return View(yapilacaklar);
         }
     }
 }
