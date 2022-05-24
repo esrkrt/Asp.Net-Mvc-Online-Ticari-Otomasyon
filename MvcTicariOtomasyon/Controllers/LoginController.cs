6240aec41985a3b6e71098b50c86352b128cbfbb
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-24 17:23:07 +0300

    login kayıt olma islemi partiallarla sağlandi

--- MvcTicariOtomasyon/Controllers/LoginController.cs
+++ MvcTicariOtomasyon/Controllers/LoginController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcTicariOtomasyon.Models.Siniflar;
 namespace MvcTicariOtomasyon.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p )
+        {
+            c.Carilers.Add(p);
+            //Mail doğrulama Gelicek
+            c.SaveChanges();
             return PartialView();
         }
     }
