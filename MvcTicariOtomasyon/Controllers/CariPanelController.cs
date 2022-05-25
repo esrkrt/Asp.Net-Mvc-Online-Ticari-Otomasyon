fe03ddb234936f638763082cd468dca88d35f637
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-25 16:36:32 +0300

    cari profilim sayfası düzenlendi ve veriler çekildi

--- MvcTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcTicariOtomasyon/Controllers/CariPanelController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcTicariOtomasyon.Models.Siniflar;
 namespace MvcTicariOtomasyon.Controllers
 {
     public class CariPanelController : Controller
     {
         // GET: CariPanel
+        Context c = new Context();
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CariMail"];
+            var degreler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            
+            return View(degreler);
         }
+       
+
     }
 }
