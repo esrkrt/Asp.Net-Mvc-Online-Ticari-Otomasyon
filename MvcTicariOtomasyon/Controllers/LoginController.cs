d06c44bd75c17bc5b508444881bffb988dd6cbe8
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-25 15:49:09 +0300

    login ekranında cari girisinde cari paneline gonderme islemi yapildi

--- MvcTicariOtomasyon/Controllers/LoginController.cs
+++ MvcTicariOtomasyon/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MvcTicariOtomasyon.Models.Siniflar;
 namespace MvcTicariOtomasyon.Controllers
 {
@@ -26,6 +27,27 @@ namespace MvcTicariOtomasyon.Controllers
             //Mail doğrulama Gelicek
             c.SaveChanges();
             return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail==p.CariMail && x.CariSifre == p.CariSifre);
+            if (bilgiler !=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
         }
     }
 }
