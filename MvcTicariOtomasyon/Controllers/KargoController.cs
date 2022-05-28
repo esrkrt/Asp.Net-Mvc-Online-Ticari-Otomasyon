87737bd4e8dbe06908d733fe40eea755cce17771
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-28 15:34:57 +0300

    rastgele takip kodu oluşturma

--- MvcTicariOtomasyon/Controllers/KargoController.cs
+++ MvcTicariOtomasyon/Controllers/KargoController.cs
@@ -18,6 +18,19 @@ namespace MvcTicariOtomasyon.Controllers
         [HttpGet]
         public ActionResult YeniKargo()
         {
+            Random rnd = new Random();
+            string[] karakterler = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "L" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, karakterler.Length);
+            k2 = rnd.Next(0, karakterler.Length);
+            k3 = rnd.Next(0, karakterler.Length);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);
+            s2 = rnd.Next(10, 99);
+            s3= rnd.Next(10, 99);
+            string kod = s1.ToString() + karakterler[k1] + s2 + karakterler[k2] + s3 + karakterler[k3];
+            ViewBag.takipkod = kod;
+
             return View();
         }
         [HttpPost]
