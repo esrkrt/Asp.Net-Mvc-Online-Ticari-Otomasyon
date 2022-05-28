b8f99e2c4347b2215350a783123f2ba6315cc86e
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-28 21:33:15 +0300

    google chartlar controller

--- MvcTicariOtomasyon/Controllers/GrafikController.cs
+++ MvcTicariOtomasyon/Controllers/GrafikController.cs
@@ -19,7 +19,7 @@ namespace MvcTicariOtomasyon.Controllers
         public ActionResult Index2()
         {
             var grafikciz = new Chart(600, 600);
-            grafikciz.AddTitle(text: " Kategoriler-Ürün Stok Sayısı").AddLegend( "Stok").AddSeries(
+            grafikciz.AddTitle(text: " Kategoriler-Ürün Stok Sayısı").AddLegend("Stok").AddSeries(
            "Değerler",
                 xValue: new[] { "Beyaz Eşya", "Televizyon", "Bilgisayar", "Küçük Ev Aletleri" },
                 yValues: new[] { 500, 250, 340, 620 }).Write();
@@ -38,9 +38,53 @@ namespace MvcTicariOtomasyon.Controllers
             var grafik = new Chart(width: 800, height: 800).AddTitle(
                 "Stoklar").AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
             return File(grafik.ToWebImage().GetBytes(), "image/jpeg");
-                
 
+        }
 
-            }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(UrunListesi(), JsonRequestBehavior.AllowGet);
+
+        }
+        public List<Sınıf1> UrunListesi()
+        {
+            List<Sınıf1> snf = new List<Sınıf1>();
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Bilgisayar",
+                Stok = 120
+            });
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Beyaz Eşya",
+                Stok = 150
+            });
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Mobilya",
+                Stok = 70
+            });
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Küçük Ev Aletleri",
+                Stok = 100
+            });
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Mobil Cihazlar",
+                Stok = 90
+            });
+            snf.Add(new Sınıf1()
+            {
+                Uurunad = "Tablet",
+                Stok = 200
+            });
+            return snf;
+        }
     }
+
 }
