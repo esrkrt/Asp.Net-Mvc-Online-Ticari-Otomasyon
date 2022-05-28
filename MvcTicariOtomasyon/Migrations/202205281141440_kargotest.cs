f5a3ac1b05d53d9a322a10e8df31ff8e8042c4f8
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-28 14:44:17 +0300

    kargo detay ve kargo takip veri tabanı olusturuldu

---
+++ MvcTicariOtomasyon/Migrations/202205281141440_kargotest.cs
@@ -0,0 +1,42 @@
+﻿namespace MvcTicariOtomasyon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class kargotest : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.KargoDetays",
+                c => new
+                    {
+                        KargoDetayİd = c.Int(nullable: false, identity: true),
+                        Aciklama = c.String(maxLength: 100, unicode: false),
+                        TakipKodu = c.String(maxLength: 10, unicode: false),
+                        Personel = c.String(maxLength: 20, unicode: false),
+                        Alici = c.String(maxLength: 20, unicode: false),
+                        Tarih = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.KargoDetayİd);
+            
+            CreateTable(
+                "dbo.KargoTakips",
+                c => new
+                    {
+                        KargoTakipİd = c.Int(nullable: false, identity: true),
+                        Aciklama = c.String(maxLength: 300, unicode: false),
+                        TakipKodu = c.String(maxLength: 10, unicode: false),
+                        Tarih = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.KargoTakipİd);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.KargoTakips");
+            DropTable("dbo.KargoDetays");
+        }
+    }
+}
---
+++ MvcTicariOtomasyon/Models/Siniflar/KargoTakip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Siniflar
+{
+    public class KargoTakip
+    {
+        [Key]
+        public int KargoTakipİd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(300)]
+        public string Aciklama { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(10)]
+        public string TakipKodu { get; set; }//1234567AB
+        public DateTime Tarih { get; set; }
+    }
+}
