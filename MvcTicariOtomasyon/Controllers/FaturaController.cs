a45fc48c3279503ef642b0fa9fb101c7ca5e3cf6
ESRA KURT <esra.kurt97833@gmail.com>
2022-05-30 02:34:25 +0300

    fatura kalemlerini ve faturalarin kaydedilmesi islemi yapildi

--- MvcTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcTicariOtomasyon/Controllers/FaturaController.cs
@@ -73,6 +73,34 @@ namespace MvcTicariOtomasyon.Controllers
             cs.deger2 = c.FaturaKalems.ToList();
             return View(cs);
         }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo,DateTime Tarih, string VergiDairesi, string Saat, string TeslimAlan, string TeslimEden, string Toplam, FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+
+            c.Faturalars.Add(f);
+
+            foreach(var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.Miktar = x.Miktar;
+                fk.FaturaID = x.FaturaID;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+        }
 
 
     }
